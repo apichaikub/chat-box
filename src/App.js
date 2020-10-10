6bd5efdc726233dc0d17b2e9e6efdb5dca6d1a89
_CHAI__ <apichai.freedom@gmail.com>
2020-10-11 06:06:16 +0700

    Set userId in localStorage.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import { v4 as uuidv4 } from 'uuid';
 import firebase from './plugins/firebase';
 import ChatBox from './components/Assemble/ChatBox';
+import { USERID } from './helpers/constant/localStorage';
 
 const Wrapper = styled.div`
   font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
@@ -11,24 +13,39 @@ const App = () => {
   
   const [messages, setMessage] = useState([])
 
+  const getUserId = () => {
+    return localStorage.getItem(USERID);
+  };
+
+  const setUserId = () => {
+    return localStorage.setItem(USERID, uuidv4());
+  };
+
   const handleSubmit = (value) => {
     if(value) {
       firebase.database().ref('/messages').push({
-        message: value
-      }); 
+        message: value,
+        userId: getUserId(),
+      });
     }
   }
 
   useEffect(() => {
-    firebase.database().ref('/messages').on('value', function(snapshot) {
+    if(!getUserId()) {
+      setUserId();
+    }
+    
+    firebase.database().ref('/messages').on('value', (snapshot) => {
       if(!snapshot.val()) return
-      const data = snapshot.val();
-      const messages = Object.keys(data).map((key) => {
+      const data = snapshot.val()
+      const keys = Object.keys(data);
+      const messages = keys.map((key) => {
+        const item = data[key]
         return {
           id: key,
           type: 'text',
-          message: snapshot.val()[key].message,
-          position: 'left'
+          message: item.message,
+          position: item.userId === getUserId() ? 'right' : 'left'
         };
       });
       setMessage(messages);
